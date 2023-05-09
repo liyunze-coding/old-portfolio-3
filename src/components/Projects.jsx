0820dcb93efa098f5c882753f6927abade23c8f8
liyunze <50455574+liyunze-coding@users.noreply.github.com>
2023-05-09 11:58:54 +0800

    changed skills section and others
    - changed PetGPT thumbnail
    - added fixed aspect ratio for images

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -37,7 +37,7 @@ export default () => {
 								</div>
 								<div className="flex items-center justify-center">
 									<img
-										className="w-3/4 md:w-1/2 mb-5 rounded-lg"
+										className="w-3/4 md:w-1/2 mb-5 rounded-lg aspect-w-16 aspect-h-9"
 										src={slide.image.src}
 										alt={slide.image.alt}
 									/>
