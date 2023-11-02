f1e7a536e185ab63d7fd2bf587ed48944665ec11
liyunze <50455574+liyunze-coding@users.noreply.github.com>
2023-11-02 09:34:28 +0800

    added Github Actions to deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,4 +6,6 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
 	integrations: [tailwind(), svelte(), react()],
+	site: "https://liyunze-coding.github.io",
+	base: "/website",
 });
